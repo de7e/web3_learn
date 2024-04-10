d6a708482b1eadb0697d5ef4be28bf16d547aa07
de7e <149045977+de7e@users.noreply.github.com>
2024-04-10 19:26:13 +0800

    Create yeye1.sol

---
+++ yeye1.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+contract yeye {
+    function returnYeye() public pure returns (string memory) {
+        return ("Hello Iam yeye!");
+    }
+}
