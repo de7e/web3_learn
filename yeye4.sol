91881bcc78990265ee2aa8b6d6c4c14edac7b059
de7e <149045977+de7e@users.noreply.github.com>
2024-04-10 19:56:00 +0800

    Update and rename yeye3.sol to yeye4.sol

--- yeye4.sol
+++ yeye4.sol
@@ -1,12 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.24;
 
-contract yeye {
+contract YEYE1 {
     function returnYeye2() public pure returns (string memory) {
         return ("Hello Iam yeye2!");
     }
 
     function returnYeye3() public pure returns (string memory) {
+        return ("Hello Iam yeye3!");
+    }
+
+contract YEYE2 {
+    function returnYeye7() public pure returns (string memory) {
+        return ("Hello Iam yeye2!");
+    }
+
+    function returnYeye8() public pure returns (string memory) {
         return ("Hello Iam yeye3!");
     }
 
