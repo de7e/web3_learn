69f5b8a34995245f3ec3c895cfb50d49f6fafd91
de7e <149045977+de7e@users.noreply.github.com>
2024-04-10 20:51:52 +0800

    Update yeye9.sol

--- yeye9.sol
+++ yeye9.sol
@@ -1,14 +1,26 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.24;
 
+function returnYea() pure returns (string memory) {
+    return ("Hello Iam yeye3!");
+}
+
+
+contract YEYE1 {
+    function returnYeye2() public pure returns (string memory) {
+        return ("Hello Iam yeye2!");
+    }
+
+    function returnYeye3() public pure returns (string memory) {
+        return ("Hello Iam yeye3!");
+    }
+}
+
 contract YEYE2 {
     function returnYeye7() public pure returns (string memory) {
         return ("Hello Iam yeye2!");
     }
-
     function returnYeye8() public pure returns (string memory) {
         return ("Hello Iam yeye3!");
     }
-
-
 }
